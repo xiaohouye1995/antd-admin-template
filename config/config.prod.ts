489f8e928afdce19809fcb99256d38557e8005e2
xiaohouye <363993481@qq.com>
2023-12-11 16:36:42 +0800

    ✨feat: 更改prod域名

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   define: {
-    API_URL: 'https://xiaohouye.cn', // API地址
+    API_URL: 'https://www.xiaohouye.cn', // API地址
   },
 });
